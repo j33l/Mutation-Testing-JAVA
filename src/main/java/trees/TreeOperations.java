8bd187694259e9c34bd0849e58df7d22422ba1f9
Jeel <45571080+j33l@users.noreply.github.com>
2022-10-12 12:18:02 -0700

    almost BFS is done

--- src/main/java/trees/TreeOperations.java
+++ src/main/java/trees/TreeOperations.java
@@ -2,7 +2,6 @@ package trees;
 
 import java.util.ArrayList; // import the ArrayList class
 
-
 // import trees.Node; // importing user defined Node class implemetation
 class Node {
     int value;
@@ -20,18 +19,18 @@ public class TreeOperations {
     
     // Breath-first search (A.K.A. Level-Order Traversal)
     // NOTE: I am using the Node implementation from `src/main/java/trees/Node.java` file, Assuming that Node.left is parent node and Node.right is child node.
-    public static <A> ArrayList<A> bfs(final Node<A> root)
+    public static ArrayList bfs(final Node root)
     {
         if (root == null) {
-            return;
+            return new ArrayList();
         }
 
-        ArrayList<Node> nodes = new LinkedList<>();
+        ArrayList<Node> nodes = new ArrayList<Node>();
         nodes.add(root);
 
         while (!nodes.isEmpty()) {
 
-            Node node = nodes.remove();
+            Node node = nodes.remove(0);
 
             System.out.print(" " + node.value);
 
@@ -43,6 +42,8 @@ public class TreeOperations {
                 nodes.add(node.right);
             }
         }
+
+        return nodes;
     }
 
     public static void main(String[] args)
@@ -57,17 +58,30 @@ public class TreeOperations {
             / \ / \
            3  4 5  6
         */
-        Node root = Node<Number>(0);
-        root.left =  Node<Number>(1);
-        root.right =  Node<Number>(2);
-        root.left.left =  Node<Number>(3);
-        root.left.right =  Node<Number>(4);
-        root.right.left =  Node<Number>(5);
-        root.right.right =  Node<Number>(6);
+        Node root = new Node(0);
+        root.left =  new Node(1);
+        root.right =  new Node(2);
+        root.left.left =  new Node(3);
+        root.left.right =  new Node(4);
+        root.right.left =  new Node(5);
+        root.right.right =  new Node(6);
 
-        System.out.println("Breadth-first search (A.K.A. Level-Order Traversal): ");
-        treeOperations.bfs(root);
+        System.out.println("Breadth-first search: ");
+        TreeOperations.bfs(root);
 
     }
 
+}
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    for (int i = 0; i < cars.size(); i++) {
+      System.out.println(cars.get(i));
+    }
+  }
 }
